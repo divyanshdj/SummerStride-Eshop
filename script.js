cf73eec91109c48174857920c9ef10bfc504e895
Divyansh Jain <124493395+divyanshdj@users.noreply.github.com>
2024-03-08 17:20:11 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+'use strict';
+
+/* copyright 2024, Divyansh Jain */
+
+/* Mobile navbar toggle */
+
+const navbar = document.querySelector("[data-navbar]");
+const navToggler = document.querySelector("[data-nav-toggler]");
+
+navToggler.addEventListener("click", function () {
+  navbar.classList.toggle("active");
+});
+
+/* Header active */
+
+const header = document.querySelector("[data-header]");
+
+window.addEventListener("scroll", function () {
+  header.classList[this.scrollY > 50 ? "add" : "remove"]("active");
+});
